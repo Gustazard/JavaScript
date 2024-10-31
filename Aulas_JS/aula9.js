e45e77ca78a92204acde2aa7a2c54385e2a25c63
Gustavo Xavier <gxavier8477@gmail.com>
2024-10-31 20:27:27 -0300

    JavaScript

--- Aulas_JS/aula9.js
+++ Aulas_JS/aula9.js
@@ -5,12 +5,18 @@ const usuario ={
     Equipe: "Ferrari"
 };
 
-const mensagem1 = "Bem-Vindo à Ferrari"
-const mensagem2 = "Bem-vindo à Williams"
-const mensagem3 = "Bem-vindo à McLaren"
-const mensagem4 = "Bem-vindo à Mercedes"
-const mensagem5 = "Bem-vindo à RedBull Racing"
-const mensagem6 = "Bem-vindo à Aston Martin"
+const mensagem1 = "Bem-Vindo à Ferrari";
+const mensagem2 = "Bem-vindo à Williams";
+const mensagem3 = "Bem-vindo à McLaren";
+const mensagem4 = "Bem-vindo à Mercedes";
+const mensagem5 = "Bem-vindo à RedBull Racing";
+const mensagem6 = "Bem-vindo à Aston Martin";
+const mensagem7 = "Bem-vindo à Alpine";
+const mensagem8 = "Bem-vindo à Toro Rosso";
+const mensagem9 = "Bem-vindo à Sauber";
+const mensagem10 = "Bem-vindo à Haas";
+
+const mensagemERRO = "ERRO! Por favor, Insira uma equipe válida.";
 
 if (usuario.Equipe === 'Ferrari'){
     console.log(`${mensagem1} ${usuario.nome} ${usuario.sobreNome}`);
@@ -29,4 +35,7 @@ else if (usuario.Equipe === 'RedBull Racing'){
 }
 else if (usuario.Equipe === 'Aston Martin'){
     console.log(`${mensagem6} ${usuario.nome} ${usuario.sobreNome}`);
+}
+else {
+console.log(`${mensagemERRO}`);
 }
